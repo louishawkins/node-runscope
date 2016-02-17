a00b97963ba3f1b2bf29bf4a41e38a3ce3152f01
Louis Hawkins <louishawkins@gmail.com>
2016-02-17 01:25:00 -0600

    update example file

--- example.js
+++ example.js
@@ -5,42 +5,50 @@ var config = {
 
 var runscope = require('./index.js')(config.token);
 //assign .bucket to work within a particular bucket
-var dashboardBucket = runscope.bucket(config.bucketKey);
+var myBucket = runscope.bucket(config.bucketKey);
 //assign .tests to work with bucket tests
-var dashboardTests = dashboardBucket.tests;
+var myBucketTests = dashboardBucket.tests;
 
 //create a test in a bucket
-dashboardTests.create({
+myBucketTests.create({
     name: 'Example Test',
     description: 'An example test created with library.'
 }).then(function (newTest) {
     //returns a new test object which has test methods
     // Now we can add steps
     // steps can be added in a chain
-    newTest.steps.addRequest({
+    newTest.steps
+        .addRequest({
             method: 'get',
             url: 'http://www.google.com',
             assert: 'status === 200'
-        }).addPause({
+        })
+        .addPause({
             duration: 2
-        }).addRequest({
+        })
+        .addRequest({
             method: 'get',
             url: 'http://www.yahoo.com',
             assert: 'status === 200'
-        }).then(function () {
+        })
+        .then(function () {
             console.log('added all the requests!');
             newTest.schedules.create({
                 environment_id: newTest.data.default_environment_id,
                 interval: '1h',
                 note: 'A schedule created through api'
-            }).then(function () {
-                newTest.trigger().then(allDone);
+            })
+            .then(newTest.trigger())
+            .wait(2000)
+            .then(newTest.results.latest())
+            .then(function (results) {
+                console.log(results);
+                process.exit(0);
+            }).catch(function (err) {
+                throw new Error(err);
             });
         })
 }).catch(function (err) {
     console.log(err);
 });
 
-function allDone () {
-    console.log('Completely finished!');
-}
